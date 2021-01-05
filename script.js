50c28ee784a2d8ea2f379be32ebab156fa07e359
Amalie Schiøtt Lauridsen <amalieschioett@MacBook-Pro-tilhrende-Amalie.local>
2021-01-05 19:23:07 +0100

    første commit

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("toggelMenu");
+    document.querySelector("#menu").classList.toggle("hide");
+    document.querySelector("#menuknap").classList.toggle("change");
+    let erSkjult = document.querySelector("#menu").classList.contains("hide");
+}
